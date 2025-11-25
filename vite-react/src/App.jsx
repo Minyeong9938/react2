b914c852c17ce069960f8b2652dc5fa31f0f4868
Minyeong9938 <isk07179@sau.ac.kr>
2025-11-25 10:20:56 +0900

    react 수업

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Box1 from './compoments/Box1'
+import Box2 from './compoments/Box2'
+
+function App() {
+
+  return (
+    <>
+      <Box1 />
+      <br />
+      <br />
+      <Box2 />
+    </>
+  )
+}
+
+export default App
